fe42cf73529311f31bccc94709eb8d65f42c045c
Riki <riki7649255@gmail.com>
2019-09-15 21:58:32 +0300

    with my changes

--- project/WebApi/Controllers/ImagesController.cs
+++ project/WebApi/Controllers/ImagesController.cs
@@ -12,7 +12,7 @@ namespace WebApi.Controllers
 {
     public class ImagesController : ApiController
     {
-
+        [Route("api/Images/GetImageById/{id}")]
         public IHttpActionResult GetImageById(int id)
         {
             COMimage img = BLLimage.GetImageById(id);
@@ -23,10 +23,19 @@ namespace WebApi.Controllers
             return Ok(img);
         }
 
-        public List<COMimage> GetImages()
+        [Route("api/Images/GetImages/{id}")]
+        public IHttpActionResult GetImages(int id)
         {
-            return BLLimage.Getimages();
-        }
+            //return BLLimage.Getimages();
+            //time is current page number in this category images
+            List<COMimage> list = BLLimage.Getimages().FindAll(img => img.UserId == id || img.UserId == 11);
+            if (list == null)
+                return BadRequest("fail to load images");
+            if (list.Count > 0)
+                return Ok(ImageMat.CreateMat(list,true));
+            else
+                return BadRequest("no more pictures for this category.");
+    }
         public IHttpActionResult GetTwelveNextImages(int id, int time)
         {
             //time is current page number in this category images
