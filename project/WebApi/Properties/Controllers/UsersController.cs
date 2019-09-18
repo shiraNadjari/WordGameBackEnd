a0bac4579c5cc0e0f4b649ae18e33f20c95687fa
Shira Nadjari <shiranadjari@gmail.com>
2019-09-18 12:37:35 +0300

    try to change ase64 to dataform

--- project/WebApi/Properties/Controllers/UsersController.cs
+++ project/WebApi/Properties/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using COMMON;
 using BLL;
+using System.Web;
 //using WindowsFormsApp1;
 
 namespace WebApi.Controllers
@@ -33,8 +34,91 @@ namespace WebApi.Controllers
         //return objects list
         //[HttpPost]
         //
-     
-        public List<COMimageObject> PostInsertImageReturnObjects([FromBody]COMimage MyBase64, int id, int catId )
+
+        //public static WebResult<int> InsertImages()
+        //{
+        //    HttpResponseMessage response = new HttpResponseMessage();
+        //    var httpRequest = HttpContext.Current.Request;
+        //    string temp = "~/UploadFile/";
+        //    string uploaded_image;
+        //    if (httpRequest.Files.Count > 0)
+        //    {
+        //        for (var i = 0; i < httpRequest.Files.Count; i++)
+        //        {
+        //            var postedFile = httpRequest.Files[i];
+        //            var filePath = HttpContext.Current.Server.MapPath(temp + postedFile.FileName);
+        //            postedFile.SaveAs(filePath);
+        //            uploaded_image = SendToStorage(filePath);
+        //            InitImageDetailsAsync(filePath);
+        //            InitImageDetailsAsync(filePath);
+        //        }
+
+        //    }
+        //    return new WebResult<int>()
+        //    {
+        //        Status = true,
+        //        Message = "Success",
+        //        Value = 1
+        //    };
+        //    //return response;
+        //}
+
+        //[HttpPost]
+        //public void PostInsertImageReturnObjects(int id, int catId)
+        //{
+        //    var httpRequest = HttpContext.Current.Request;
+
+        //   var re= httpRequest.GetBufferlessInputStream();
+        //    ////=httpRequest.Params["image_data"];
+        //   // re = httpRequest.Form["image_data"];
+
+        //    string temp = "~/UploadFile/";
+        //    string uploaded_image;
+        //    if (httpRequest.Files.Count > 0)
+        //    {
+        //        for (var i = 0; i < httpRequest.Files.Count; i++)
+        //        {
+        //            var postedFile = httpRequest.Files[i];
+        //            var filePath = HttpContext.Current.Server.MapPath(temp + postedFile.FileName);
+        //            postedFile.SaveAs(filePath);
+        //            //List < COMimageObject >
+
+        //        }
+        //    }
+        //    /////
+        //    string fileName = string.Empty;
+        //    try
+        //    {
+        //        if (HttpContext.Current.Request.Files.AllKeys.Any())
+        //        {
+        //            // Get the uploaded image from the Files collection
+        //            var httpPostedFile = HttpContext.Current.Request.Files["image_data"];
+        //            fileName = httpPostedFile.FileName;
+        //        }
+        //        //if (httpPostedFile != null)
+        //        //{
+        //        //    // OBtient le path du fichier 
+        //        //    fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/UploadedFiles"), httpPostedFile.FileName);
+
+        //        //    // Sauvegarde du fichier dans UploadedFiles sur le serveur
+        //        //    httpPostedFile.SaveAs(fileSavePath);
+        //        //}
+
+        //        //string base64 = MyBase64.URL.Substring(23);
+        //        COMimage img = new COMimage();
+        //        img.UserId = id;
+        //        img.CategoryID = catId;
+        //        img.URL = "";
+        //        //List<COMimageObject> objs = BLLimage.GetImageFromUserReturnObjectsList(img, MyBase64.URL);
+
+        //        // return objs;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw;
+        //    }
+        //}
+        public List<COMimageObject> PostInsertImageReturnObjects([FromBody]COMimage MyBase64, int id, int catId)
         {
             try
             {
@@ -44,7 +128,7 @@ namespace WebApi.Controllers
                 img.CategoryID = catId;
                 img.URL = "";
                 List<COMimageObject> objs = BLLimage.GetImageFromUserReturnObjectsList(img, MyBase64.URL);
-               
+
                 return objs;
             }
             catch (Exception)
@@ -52,7 +136,7 @@ namespace WebApi.Controllers
                 throw;
             }
         }
-      
+
         public static void PostObjects([FromBody] List<COMimageObject> objs, int id,int catid)
         {
                
